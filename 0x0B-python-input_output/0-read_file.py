8dc658941cca75fb92fbac14ae7246a365da9228
Richard <rclancing@gmail.com>
2023-01-12 05:31:37 +0100

    task 0

---
+++ 0x0B-python-input_output/0-read_file.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""A module that reads a file and prints it"""
+
+
+def read_file(filename=""):
+    """A function that reads a file and prints it"""
+
+    if type(filename) is str:
+        with open(filename, mode="r", encoding="utf-8") as f_name:
+            print(f_name.read(), end="")
