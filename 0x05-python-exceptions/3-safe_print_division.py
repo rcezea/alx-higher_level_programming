04963a24556c9cada7b6bc61cc5255c63e708eaa
Richard <rclancing@gmail.com>
2023-01-21 09:13:28 -0800

    task 3

--- 0x05-python-exceptions/3-safe_print_division.py
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -6,7 +6,7 @@ def safe_print_division(a, b):
     except ZeroDivisionError:
         result = None
     finally:
-        if result != None:
+        if result is not None:
             print("Inside result: {:.1f}".format(result))
             return result
         print("Inside result: None")
