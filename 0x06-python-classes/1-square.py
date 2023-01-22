7993fc58a6aed9cbab04ab44403536c65deb5cae
Richard <rclancing@gmail.com>
2023-01-22 07:56:07 -0800

    task 1

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+
+""" Write a class Square that defines a square """
+
+
+class Square:
+    """ initialize square with size of square """
+    def __init__(self, size):
+        self.__size = size
