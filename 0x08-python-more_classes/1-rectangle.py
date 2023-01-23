c30087ff8df0f0283f29aae2de8c18a258a7dd70
Richard <rclancing@gmail.com>
2023-01-23 15:44:46 +0100

    task 1

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -5,19 +5,17 @@ by: (based on 1-rectangle.py)"""
 
 class Rectangle:
     """Rectangle class succesfully created"""
+    
     def __init__(self, width=0, height=0):
-        """initializes the private attributes"""
         self.__width = width
         self.__height = height
 
     @property
     def width(self):
-        """returns the value for width"""
         return self.__width
 
     @width.setter
     def width(self, value):
-        """sets the value for width"""
         if type(value) is not int:
             raise TypeError("width must be an integer")
         if value < 0:
@@ -26,12 +24,10 @@ class Rectangle:
 
     @property
     def height(self):
-        """returns the value for height"""
         return self.__height
 
     @height.setter
     def height(self, value):
-        """sets the value for height"""
         if type(value) is not int:
             raise TypeError("height must be an integer")
         if value < 0:
