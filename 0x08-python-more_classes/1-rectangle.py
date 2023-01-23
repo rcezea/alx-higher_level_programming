96df222d704c66627edf020124aae73bb3e6ae10
Richard <rclancing@gmail.com>
2023-01-23 15:48:57 +0100

    task 1

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -1,9 +1,8 @@
 #!/usr/bin/python3
-"""Write a class Rectangle that defines a rectangle
-by: (based on 1-rectangle.py)"""
+"""Write a class Rectangle"""
 
 
-class Rectangle:
+class Rectangle(object):
     """Rectangle class succesfully created"""
     def __init__(self, width=0, height=0):
         self.__width = width
