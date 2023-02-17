2e039449aea7f94a099581b04749543241100408
rcezea <rclancing@gmail.com>
2023-02-17 20:45:05 +0100

    task 14

--- 0x0E-SQL_more_queries/14-my_genres.sql
+++ 0x0E-SQL_more_queries/14-my_genres.sql
@@ -2,10 +2,6 @@
 SELECT tvg.name
 FROM tv_genres AS tvg INNER JOIN tv_show_genres AS tsg
 ON tsg.genre_id = tvg.id
-/*WHERE tsg.show_id = (
-    SELECT id FROM tv_shows AS ts
-    WHERE ts.title = 'Dexter'
-)*/
 INNER JOIN tv_shows AS ts
 ON tsg.show_id = ts.id
 WHERE ts.title = 'Dexter'
