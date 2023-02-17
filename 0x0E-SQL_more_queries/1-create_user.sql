b85127f7d893e7b2657be11291e23b33a2fe69c2
rcezea <rclancing@gmail.com>
2023-02-17 14:21:36 +0100

    task 1

--- 0x0E-SQL_more_queries/1-create_user.sql
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -1,4 +1,5 @@
 -- create new user with all privileges
-CREATE USER 'user_0d_1'@'localhost' IF NOT EXISTS
-IDENTIFIED BY 'user_0d_1_pwd';
+CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost'
+IDENTIFIED WITH mysql_native_password
+BY 'user_0d_1_pwd';
 GRANT ALL PRIVILEGES ON *.* TO user_0d_1@localhost;
