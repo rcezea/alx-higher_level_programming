8d48227ccf304e90ecb2a3bcabb0177d3140b255
rcezea <rclancing@gmail.com>
2023-02-17 15:21:29 +0100

    task 6

--- 0x0E-SQL_more_queries/6-states.sql
+++ 0x0E-SQL_more_queries/6-states.sql
@@ -1,6 +1,7 @@
 -- create database hbtn_0d_usa with table states
 CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
 CREATE TABLE IF NOT EXISTS states(
-    id INT UNIQUE AUTO_INCREMENT NOT NULL PRIMARY KEY,
+    PRIMARY KEY(id)
+    id INT AUTO_INCREMENT NOT NULL,
     name VARCHAR(256) NOT NULL
 );
