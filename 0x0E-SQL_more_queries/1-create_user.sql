8f4bde938937082cf3f5de1be711559fa773cf3c
rcezea <rclancing@gmail.com>
2023-02-17 14:18:05 +0100

    task 1

--- 0x0E-SQL_more_queries/1-create_user.sql
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -2,4 +2,4 @@
 CREATE USER 'user_0d_1'@'localhost' IF NOT EXISTS
 IDENTIFIED WITH mysql_native_password
 BY 'user_0d_1_pwd';
-GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost';
+GRANT ALL PRIVILEGES ON *.* TO user_0d_1@localhost;
