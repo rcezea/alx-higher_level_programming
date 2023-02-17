9481789f9cf2ad1ebc4e53a875c82f1930b571bb
rcezea <rclancing@gmail.com>
2023-02-17 16:27:16 +0100

    task 7

--- 0x0E-SQL_more_queries/7-cities.sql
+++ 0x0E-SQL_more_queries/7-cities.sql
@@ -1,7 +1,9 @@
 -- create database hbtn_0d_usa with table cities
 CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
 CREATE TABLE IF NOT EXISTS hbtn_0d_usa.cities(
-    id INT UNIQUE NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    state_id INT NOT NULL FOREIGN KEY,
-    name VARCHAR(256) NOT NULL
+    id INT UNIQUE NOT NULL AUTO_INCREMENT,
+    state_id INT NOT NULL,
+    name VARCHAR(256) NOT NULL,
+    PRIMARY KEY(id),
+    FOREIGN KEY(state_id)
 );
