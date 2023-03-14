5905e088939726c62d8c4d4571f9c51efddcf292
rcezea <rclancing@gmail.com>
2023-03-14 05:04:09 +0100

    task 2

---
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+
+require('process');
+
+if (process.argv.length - 2 === 0){
+    console.log("No argument");
+}else if (process.argv.length - 2 === 1){
+    console.log("Argument found");
+}else{
+    console.log("Arguments found");
+};
