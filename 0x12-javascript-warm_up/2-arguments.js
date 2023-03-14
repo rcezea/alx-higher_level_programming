e4bd4b4f8a5228992a9365f9f77a65cd5c42e7f6
rcezea <rclancing@gmail.com>
2023-03-14 05:12:08 +0100

    semistandard fix

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -2,10 +2,10 @@
 
 require('process');
 
-if (process.argv.length - 2 === 0){
-    console.log("No argument");
-}else if (process.argv.length - 2 === 1){
-    console.log("Argument found");
-}else{
-    console.log("Arguments found");
-};
+if (process.argv.length - 2 === 0) {
+  console.log('No argument');
+} else if (process.argv.length - 2 === 1) {
+  console.log('Argument found');
+} else {
+  console.log('Arguments found');
+}
