d072f0724fa53954e31ea806c2b8a530340e49a5
rcezea <rclancing@gmail.com>
2023-03-18 06:27:36 +0100

    task 0

---
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+
+import MySQLdb
+from sys import argv
+
+def main():
+
+    db = MySQLdb.Connect(host="localhost", user=argv[1], password=argv[2], db=argv[3], port=3306)
+
+    cur = db.cursor()
+
+    cur.execute("SELECT * FROM states ORDER BY id DESC")
+
+    rows = cur.fetchall()
+
+    for row in rows:
+        print(row)
+
+if __name__ == '__main__':
+    main()
