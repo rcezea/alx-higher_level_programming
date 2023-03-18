ccb524eb36bd52f1af0f5a66792a9e636d3d761f
rcezea <rclancing@gmail.com>
2023-03-18 13:12:06 +0100

    task 1

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -1,5 +1,8 @@
 #!/usr/bin/python3
-
+"""
+Write a script that lists all states with a name\
+starting with N (upper N) from the database hbtn_0e_0_usa
+"""
 import MySQLdb
 import sys
 
@@ -7,4 +10,4 @@ if __name__ == '__main__':
     db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
     cur = db.cursor()
     cur.execute("SELECT * FROM states ORDER BY id")
-    [print (row) for row in cur.fetchall() if row[1][0] == 'N']
+    [print(row) for row in cur.fetchall() if row[1][0] == 'N']
