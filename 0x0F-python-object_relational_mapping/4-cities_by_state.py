bad894166184f699879dce637ebe89e7c81f44f1
rcezea <rclancing@gmail.com>
2023-03-18 14:15:30 +0100

    task 4

---
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""
+Write a script that lists all states with a name\
+starting with N (upper N) from the database hbtn_0e_0_usa
+"""
+import MySQLdb
+import sys
+
+if __name__ == '__main__':
+    db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
+    cur = db.cursor()
+    cur.execute("SELECT * FROM cities ORDER BY id")
+    [print(row) for row in cur.fetchall()]
