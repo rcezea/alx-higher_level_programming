45b8c5370fb1347324a9bf48a96fe0158b98ee26
rcezea <rclancing@gmail.com>
2023-03-18 13:27:30 +0100

    task 3

---
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""
+Write a script that takes in an argument\
+and displays all values in the states table of hbtn_0e_0_usa\
+where name matches the argument
+"""
+import MySQLdb
+import sys
+
+if __name__ == '__main__':
+    db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
+    name = sys.argv[4]
+    cur = db.cursor()
+    cur.execute("SELECT * FROM states WHERE name LIKE '{}'".format(name))
+    for row in cur.fetchall():
+        if row[1] == name:
+            print(row)
