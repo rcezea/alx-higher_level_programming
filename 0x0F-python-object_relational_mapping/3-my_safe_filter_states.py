0c53caa1e80e70618ea88905daa039e0bc8026a5
rcezea <rclancing@gmail.com>
2023-03-18 14:04:54 +0100

    task 3

--- 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -14,5 +14,4 @@ if __name__ == '__main__':
     cur.execute("SELECT * FROM states WHERE name LIKE '{}'\
                 ORDER BY id ASC".format(name))
     for row in cur.fetchall():
-        if row[1] == name:
-            print(row)
+        print(row)
