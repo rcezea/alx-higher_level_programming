1b9ecda96af4124dd2fdb55794d611572244222a
rcezea <rclancing@gmail.com>
2023-03-23 15:47:51 +0100

    Advanced task 1

---
+++ 0x0F-python-object_relational_mapping/100-relationsip_states_cities.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+'''
+script that creates the State
+“California” with the City “San
+Francisco” from the database hbtn_0e_100_usa
+'''
+
+from sys import argv
+from relationship_city import City, Base, eng
+from sqlalchemy.orm import Session
+from relationship_state import State
+from sqlalchemy import create_engine
+
+if __name__ == '__main__':
+    engine = create_engine(f"mysql+mysqldb://{argv[1]}:{argv[2]}@localhost:3306/{argv[3]}")
+    session =  Session
+    s1 = State(name='California')
+    c1 = City(name='San Francisco')
+    s1.cities.append(c1)
+    session.add_all([s1, c1])
+    session.commit()
---
+++ 0x0F-python-object_relational_mapping/relationship_city.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""
+improve model_city
+"""
+from sqlalchemy import Column, Integer, String, ForeignKey
+from sqlalchemy.ext.declarative import declarative_base
+
+Base = declarative_base()
+
+
+class City(Base):
+    """ class named City"""
+    __tablename__ = "cities"
+    id = Column(Integer(), nullable=False, primary_key=True)
+    name = Column(String(128), nullable=False)
+    state_id = Column(Integer(), ForeignKey("states.id"), nullable=False)
