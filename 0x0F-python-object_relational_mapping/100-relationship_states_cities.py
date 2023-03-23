17d8dc74965fa7c445304226ec38a1dedbedb7c1
rcezea <rclancing@gmail.com>
2023-03-23 15:53:15 +0100

    Advanced task 1

--- 0x0F-python-object_relational_mapping/100-relationship_states_cities.py
+++ 0x0F-python-object_relational_mapping/100-relationship_states_cities.py
@@ -12,8 +12,9 @@ from relationship_state import State
 from sqlalchemy import create_engine
 
 if __name__ == '__main__':
-    engine = create_engine(f"mysql+mysqldb://{argv[1]}:{argv[2]}@localhost:3306/{argv[3]}")
-    session =  Session
+    engine = create_engine(f"mysql+mysqldb:\
+                           //{argv[1]}:{argv[2]}@localhost:3306/{argv[3]}")
+    session = Session
     s1 = State(name='California')
     c1 = City(name='San Francisco')
     s1.cities.append(c1)
