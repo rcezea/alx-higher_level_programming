848c352ae54a55f9ea334cb8430e8c8f2624c2dc
rcezea <rclancing@gmail.com>
2023-04-10 09:43:38 +0100

    task 0

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,12 +1,13 @@
 #!/usr/bin/python3
 # Fetch a website
 
-import urllib.request
-''' This module is used to fetch html content'''
+if __name__ == '__main__':
+    import urllib.request
 
-with urllib.request.urlopen('https://alx-intranet.hbtn.io/status') as response:
-    content = response.read()
-print("Body response:")
-print("\t- type: {}".format(type(content)))
-print("\t- content: {}".format(content))
-print("\t- utf8 content: {}".format(content.decode('utf-8')))
+    with urllib.request.urlopen('https://alx-intranet.hbtn.io/status') as response:
+        html = response.read()
+    string = f"Body response:\n\
+        - type: {type(html)}\n\
+        - content: {html}\n\
+        - utf8 content: {html.decode('utf-8')}"
+    print(string)
