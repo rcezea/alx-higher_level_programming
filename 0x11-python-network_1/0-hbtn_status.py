93de64a18434a744e8722061370d47406ff6cb92
rcezea <rclancing@gmail.com>
2023-04-10 09:29:59 +0100

    task 0

---
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+# Fetch a website
+
+import urllib.request
+
+with urllib.request.urlopen('https://alx-intranet.hbtn.io/status') as response:
+    html = response.read()
+string = f"Body response:\n\
+    \t- type: {type(html)}\n\
+    \t- content: {html}\n\
+    \t- utf8 content: {html.decode('utf-8')}"
+print(string)
