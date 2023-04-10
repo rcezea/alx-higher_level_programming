de7068aec1956b0b1e0d30314abe72d16caa1af8
rcezea <rclancing@gmail.com>
2023-04-10 09:46:18 +0100

    task 0

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -2,9 +2,9 @@
 ''' This module is used to fetch html content'''
 
 if __name__ == '__main__':
-    import urllib.request
+    import urllib.request as re
 
-    with urllib.request.urlopen('https://alx-intranet.hbtn.io/status') as response:
+    with re.urlopen('https://alx-intranet.hbtn.io/status') as response:
         html = response.read()
     string = f"Body response:\n\
         - type: {type(html)}\n\
