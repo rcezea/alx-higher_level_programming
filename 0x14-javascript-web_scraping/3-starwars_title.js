45f8c388dd6e70fa5ecb6183cfc30405c051a1bf
rcezea <rclancing@gmail.com>
2023-04-25 15:17:23 +0100

    task 3

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -1,12 +1,11 @@
 #!/usr/bin/node
 const request = require('request');
-const url = `https://swapi-api.alx-tools.com/api/films/${process.argv[2]}`
+const url = `https://swapi-api.alx-tools.com/api/films/${process.argv[2]}`;
 require('process');
 request.get(url, function (error, response, body) {
-    if (error) {
-      console.log(error);
-    } else {
-      console.log(JSON.parse(body).title);
-    }
-  });
-  
+  if (error) {
+    console.log(error);
+  } else {
+    console.log(JSON.parse(body).title);
+  }
+});
