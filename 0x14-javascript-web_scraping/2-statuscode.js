3bce5fa3b6750401eeb8cfda4a54f6351db0cc75
rcezea <rclancing@gmail.com>
2023-04-25 14:39:29 +0100

    task 2

--- 0x14-javascript-web_scraping/2-statuscode.js
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -1,6 +1,9 @@
 #!/usr/bin/node
 const request = require('request');
 require('process');
+if (process.argv.length - 2 == 0) {
+    return;
+}
 request(process.argv[2], function (error, response) {
   console.log('code: ', response.statusCode);
 });
