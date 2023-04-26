d18261091022f83ce2470db7f25f8fb66b7fb795
rcezea <rclancing@gmail.com>
2023-04-26 12:30:46 +0100

    task 4

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -6,15 +6,15 @@ request.get(url, function (error, response, body) {
   if (error) {
     console.log(error);
   }
-    const films = JSON.parse(body).results;
-    let count = 0;
-    for (const film of films) {
-        for (const character of film.characters) {
-            if (character.endsWith('18/')) {
-                count += 1;
-                break;
-            }
-        }
+  const films = JSON.parse(body).results;
+  let count = 0;
+  for (const film of films) {
+    for (const character of film.characters) {
+      if (character.endsWith('18/')) {
+        count += 1;
+        break;
+      }
     }
+  }
   console.log(count);
 });
