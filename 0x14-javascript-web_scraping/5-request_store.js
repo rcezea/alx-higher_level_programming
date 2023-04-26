af676d2a9d46ca64810f0cfad135062f3facc8a6
rcezea <rclancing@gmail.com>
2023-04-26 12:46:33 +0100

    task 5

--- 0x14-javascript-web_scraping/5-request_store.js
+++ 0x14-javascript-web_scraping/5-request_store.js
@@ -4,13 +4,13 @@ require('process');
 const url = process.argv[2];
 fs = require('fs');
 request(url, function (err, resp, body) {
-    if (err) {
-        console.log(err)
-    } else {
-        fs.writeFile(process.argv[3], body, err => {
-            if (err) {
-              console.log(err);
-            }
-          });
-    }
-})
+  if (err) {
+    console.log(err);
+  } else {
+    fs.writeFile(process.argv[3], body, err => {
+      if (err) {
+        console.log(err);
+      }
+    });
+  }
+});
