feeb2ecd67f08c84aa439daaa3dacdf76bcec987
Richard <rcezea007@gmail.com>
2025-02-01 23:53:37 +0100

    0. Integers addition

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -1,11 +1,37 @@
-#!/usr/bin/python3
+"""
+Function that adds two integers
+"""
 
 def add_integer(a, b=98):
-    valuetype = (int, float)
-    if not isinstance(a, valuetype):
+    """
+    Function that adds two integers
+
+    >>> add_integer(1, 2)
+    3
+    >>> add_integer(2, 3)
+    5
+    >>> add_integer(3, 4)
+    7
+    >>> add_integer(4, 'a')
+    Traceback (most recent call last):
+            ...
+    TypeError: b must be an integer
+
+    >>> add_integer('b', 6)
+    Traceback (most recent call last):
+            ...
+    TypeError: a must be an integer
+    """
+    types = [float, int]
+
+    if not type(a) in types:
         raise TypeError("a must be an integer")
-    if not isinstance(b, valuetype):
+
+    if not type(b) in types:
         raise TypeError("b must be an integer")
-    a = int(a)
-    b = int(b)
-    return a+b
+
+    return int(a) + int(b)
+
+if __name__ == "__main__":
+    import doctest
+    doctest.testmod()
