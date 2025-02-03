f26ae394c7a96c8d57d11ccbf2554bee863cbf50
Richard <rcezea007@gmail.com>
2025-02-03 10:47:32 +0100

    0. Integers addition

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -4,20 +4,19 @@ Function that adds two integers
 """
 
 
-def add_integer( a, b = 98 ):
+def add_integer(a, b=98):
     """
     Function that adds two integers
     """
-
     if a is None:
-        raise TypeError( "add_integer() missing 1 required positional argument: 'a'" )
+        raise TypeError("add_integer() missing 1 required positional argument: 'a'")
 
-    types = [ float, int ]
+    types = [float, int]
 
-    if not type( a ) in types:
-        raise TypeError( "a must be an integer" )
+    if not type(a) in types:
+        raise TypeError("a must be an integer")
 
-    if not type( b ) in types:
-        raise TypeError( "b must be an integer" )
+    if not type(b) in types:
+        raise TypeError("b must be an integer")
 
-    return int( a ) + int( b )
+    return int(a) + int(b)
