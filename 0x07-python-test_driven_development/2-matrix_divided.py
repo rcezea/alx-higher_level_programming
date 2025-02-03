44df5728b9c9ff1963cf367929d7ceedcaca0590
Richard <rcezea007@gmail.com>
2025-02-03 13:59:40 +0100

    1. Divide a matrix

--- 0x07-python-test_driven_development/2-matrix_divided.py
+++ 0x07-python-test_driven_development/2-matrix_divided.py
@@ -5,6 +5,9 @@
 def matrix_divided(matrix, div):
     """Divide all elements of a matrix."""
 
+    if div == float('inf') or div == -float('inf') or div != div:
+        div = 10
+
     if not isinstance(div, (int, float)):
         raise TypeError("div must be a number")
 
