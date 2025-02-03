16e9a3406f2fc4d030b2026baff047194271cccb
Richard <rcezea007@gmail.com>
2025-02-03 06:18:06 +0100

    0. Integers addition

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -1,3 +1,4 @@
+#!/usr/bin/python3
 """
 Function that adds two integers
 """
@@ -5,24 +6,9 @@ Function that adds two integers
 def add_integer(a, b=98):
     """
     Function that adds two integers
-
-    >>> add_integer(1, 2)
-    3
-    >>> add_integer(2, 3)
-    5
-    >>> add_integer(3, 4)
-    7
-    >>> add_integer(4, 'a')
-    Traceback (most recent call last):
-            ...
-    TypeError: b must be an integer
-
-    >>> add_integer('b', 6)
-    Traceback (most recent call last):
-            ...
-    TypeError: a must be an integer
     """
     types = [float, int]
+
 
     if not type(a) in types:
         raise TypeError("a must be an integer")
